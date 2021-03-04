588421e25715dbc082cf9c0447b101fc08c75270
Eldar <dryga300@gmail.com>
2021-03-04 12:18:25 +0200

    My 4-th page

---
+++ site/SpeakHello.js
@@ -0,0 +1,11 @@
+(function () {
+
+ var helloSpeaker ={};
+var  speakWord  =  "Hi " ;
+
+helloSpeaker.speak = function( name )  {
+ console.log(speakWord  +  " "  +  name );
+}
+
+window.helloSpeaker = helloSpeaker;
+})();
